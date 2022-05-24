dc6be43e62c529ac6b00dafb3b601e35c018b657
Shengwei LI <antoniovvtonyvv@gmail.com>
2022-05-24 15:37:51 +0200

    1 version

---
+++ ssg/content.py
@@ -0,0 +1,48 @@
+from pickle import NONE
+import re
+from yaml import load , FullLoader
+from collections.abc import Mapping
+
+class Content(Mapping):
+    __delimeter = "^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimeter, re.MULTILINE)
+
+    @classmethod
+    def load(cls, string):
+        _, fm, content = cls.__regex.split(string, 2)
+        load(fm, Loader=FullLoader)
+
+    def __init__(self, metadata, content):
+        self.data = metadata
+        self.data["content"] = content
+
+    @property
+    def body(self):
+        return self.data["content"]
+
+    @property
+    def type(self):
+        if self.data.has_key("type"):
+            return self.data["type"]
+        else:
+            return NONE
+
+    @type.setter
+    def type(self, new_type):
+        self.data["type"] = new_type
+    
+    def __getitem__(self, key) :
+        return self.data[key]
+
+    def __iter__(self):
+        return self.data
+
+    def __len__(self) :
+        return len(self.data)
+    
+    def __repr__(self):
+        data = []
+        for key, value in self.data.items():
+            if not key == "content":
+                data[key] = value
+        return str(data)
